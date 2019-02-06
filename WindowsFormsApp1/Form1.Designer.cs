f6e961e86ff97b8eee2f3e644789d8dd13880fdd
unknown <be_siktas_1903@hotmail.com>
2019-02-06 09:30:55 +0300

    gonderme

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(22, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(607, 280);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "BASLA{\r\nyazdir(\"Merhaba\");\r\n}";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(511, 298);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 37);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Başla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(162)));
+            this.textBox2.Location = new System.Drawing.Point(22, 350);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(607, 139);
+            this.textBox2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 501);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Derleyici ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public int onay = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            string s = textBox1.Text.Trim();
+            string[] aranacakdeger = { "yazdir","oku"};
+            var say = 0;
+            string[] kelimeler = s.Split(';');
+            int top = kelimeler.Length - 1;
+            if (s.Substring(0,6)=="BASLA{" && kelimeler[kelimeler.Length-1]=="\r\n}")
+            {
+
+            for (int k = 0;k < top; k++)
+            {
+                for (int i = 0; i < aranacakdeger.Length; i++)
+                {
+
+               
+            int deger = s.IndexOf(aranacakdeger[i],say);
+                if (deger!=-1)
+                {
+            
+            var ara = s.IndexOf(";",say+1);
+            if (ara!=-1)
+            {
+                       
+                    say = ara;
+                        if (i == 0)
+                        {
+                            string al = s.Substring(deger, ara - deger) + ";";
+
+                            var ilk = al.IndexOf("\"");
+                            var ikinci = al.IndexOf("\"", ilk + 1);
+                                    if (ilk < 0 && ikinci < 0)
+                                    {
+                                        textBox2.Text = "Yazılan komut eksik veya yanlış...";
+                                        return;
+                                    }
+                           var sonuc = al.Substring(ilk + 1, ikinci - ilk - 1);
+
+
+                            if ("yazdir(\"" + sonuc + "\");" == al)
+                            {
+                                        if (textBox2.Text=="")
+                                        {
+                                            textBox2.Text += sonuc;
+                                        }
+                                        else
+                                        {
+                                            textBox2.Text += "\r\n" + sonuc;
+                                        }
+                               
+                            }
+                        }else if (i == 1) {
+                           
+                            string al = s.Substring(deger, ara - deger) + ";";
+                                var ilk = al.IndexOf("\"");
+                                var ikinci = al.IndexOf("\"", ilk + 1);
+                                    if (ilk<0 && ikinci<0)
+                                    {
+                                        textBox2.Text = "Yazılan komut eksik veya yanlış...";
+                                        return;
+                                    }
+                                var sonuc = al.Substring(ilk + 1, ikinci - ilk - 1);
+                                     if ("oku(\"" + sonuc + "\");" == al)
+                                     {
+                                                   fOkuyucu ff = new fOkuyucu();
+                                                   ff.label1.Text = sonuc;
+                                                   ff.ShowDialog();
+                                                    if (textBox2.Text=="")
+                                                    {
+                                                        textBox2.Text += ff.txtdeger.Text;
+                                                    }
+                                                    else
+                                                    {
+                                                        textBox2.Text += "\r\n" + ff.txtdeger.Text;
+                                                    }
+                                       
+
+                                    }
+                                    
+                        }
+                        else
+                       {
+                        textBox2.Text = "Yazılan komut eksik veya yanlış...";
+                        }
+            }else if (ara + 2 == s.Length)
+              {
+                  return;
+              }
+            else
+            {
+                textBox2.Text = "Yazılan komut eksik veya yanlış...";
+            }
+                }
+            }
+          }
+            }
+            else
+            {
+               textBox2.Text = "Yazılan komut eksik veya yanlış...";
+            }
+
+        }
+
+       
+  }
+}
